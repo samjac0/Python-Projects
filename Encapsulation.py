72b81001fbb60d5c4ecc1a62cbaaa2e03ff1652d
samjac0 <98563031+samjac0@users.noreply.github.com>
2022-04-19 13:23:44 -0700

    Update Encapsulation.py

--- Encapsulation.py
+++ Encapsulation.py
@@ -1,7 +1,10 @@
 # This simple excersise denotes encapsulation
 class secretToLife:
     def __init__(self):
-        self.__privateStr = "The secret to life is 42" #first protected variable
+        self.__privateStr = "The secret to life is 42" #private variable
+
+    def getProtected(self):
+        self._protectedStr = "0" #protected variable
 
     def getPrivate(self):
         print(self.__privateStr)
@@ -10,6 +13,8 @@ class secretToLife:
         self.__privateStr = private
 
 obj = secretToLife()
+obj._protectedStr = 1337
+print(obj._protectedStr)
 obj.getPrivate()
-obj.setPrivate("Oops, did I just say that out loud? 0_0") #second set
+obj.setPrivate("Oops, did I just say that out loud? 0_0") #second set of the private variable
 obj.getPrivate()
