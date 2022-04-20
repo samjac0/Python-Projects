33bf3b15654b887ca1ec7c10d663146a95dbca23
samjac0 <98563031+samjac0@users.noreply.github.com>
2022-04-19 20:40:50 -0700

    Web Generator

--- Web Page Generator/functions.py
+++ Web Page Generator/functions.py
@@ -1,21 +1,28 @@
 import webbrowser
+from tkinter import *
 import tkinter as tk
 import webPageGenerator
 
 def createWebsite(self):
+    var_subbmittedText = self.labelTxt.get()
     name = open("webGenerator.html", "w")
     name.write("""
     <html>
         <body>
             <h1>
-            Stay tuned for our amazing summer sale!
+            {}
             </h1>
         </body>
-    </html> """)
+    </html> """.format(var_subbmittedText))
     name.close()
 
+#This lauches a window on button click
 def viewWebsite(self):
     webbrowser.open_new("webGenerator.html")
 
+
+
+
 if __name__ == '__main__':
-    main()
+    pass
+
--- Web Page Generator/webPageGenerator.py
+++ Web Page Generator/webPageGenerator.py
@@ -8,20 +8,21 @@ import functions
 class window(Frame):
     def __init__(self):
         tk.Frame.__init__(self)
-        self.pack()
         self.master.title("Web Generator")
-# Textbox line
+# Textbox description heading
+        self.lblTitle = tk.Label(self.master,text='Use this text box to update your website text:')
+        self.lblTitle.grid(row=0,column=0,columnspan=2,padx=(27,0),pady=(10,0),sticky=N+W)
+# Textbox 
         self.labelTxt = tk.Entry(self.master,text='')
-        self.Txt.grid(row = 1,column = 0,rowspan = 2,columnspan = 1,padx = (30,40),pady = (0,0),sticky = N+E+W)
-
+        self.labelTxt.grid(row = 1,column = 0,rowspan = 3,columnspan = 3,padx = (30,40),pady = (0,10),sticky = N+E+W)
 # "Submit changes" button
-        self.button1 = Button(self, text = "Sumbit changes", width = 25,
+        self.button1 = Button(self.master, text = "Sumbit changes", width = 25,
                                command=lambda: functions.createWebsite(self))
-        self.button1.grid( row = 3, column = 1, columnspan = 1, sticky = W+E+N+S)
+        self.button1.grid( row = 4, column = 0, columnspan = 1, sticky = W+E+N+S)
 # "View" button
-        self.button2 = Button(self, text = "View", width = 25,
+        self.button2 = Button(self.master, text = "View", width = 25,
                                command=lambda: functions.viewWebsite(self))
-        self.button2.grid( row = 3, column = 2, columnspan = 1, sticky = W+E+N+S)
+        self.button2.grid( row = 4, column = 1, columnspan = 1, sticky = W+E+N+S)
 
     #def close_window(self):
         #self.destroy()
