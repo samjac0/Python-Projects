0a5a5c488a2a019f69f7baa7b47fb4a90c04d057
samjac0 <98563031+samjac0@users.noreply.github.com>
2022-04-26 16:08:52 -0700

    django app

---
+++ Django Projects/bankproject/Django_Checkbook/Checkbook/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='index'),
+    path('create/', views.create_account, name='create'),
+    path('balance/', views.balance, name='balance'),
+    path('transaction/', views.transaction, name='transaction'),
+    path('<int:pk>/balance/', views.balance, name='balance'),
+]
